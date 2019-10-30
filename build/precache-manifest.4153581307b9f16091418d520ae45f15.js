e682e80f600fef5dafac12225db2d635d9979be0
Aleksandr Krasikov <russkiy78@gmail.com>
2019-10-30 22:15:45 +0300

    Add files via upload

---
+++ build/precache-manifest.4153581307b9f16091418d520ae45f15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871b6d3abd13054d44584b986bacbdfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a2b87279a690c01d0b8",
+    "url": "/static/css/2.da99ac71.chunk.css"
+  },
+  {
+    "revision": "3ad2e499d7231bc2994a",
+    "url": "/static/css/main.ebdf5a48.chunk.css"
+  },
+  {
+    "revision": "1a2b87279a690c01d0b8",
+    "url": "/static/js/2.24db0457.chunk.js"
+  },
+  {
+    "revision": "3ad2e499d7231bc2994a",
+    "url": "/static/js/main.759192d0.chunk.js"
+  },
+  {
+    "revision": "314fdd0dbddaa27cb73d",
+    "url": "/static/js/runtime-main.3d47341c.js"
+  },
+  {
+    "revision": "e524ff8f8c9b8c2d977c38204a92765e",
+    "url": "/static/media/logo_comlab.e524ff8f.png"
+  },
+  {
+    "revision": "a9dfbf119530f5b7ba0ec040c5fdbe83",
+    "url": "/static/media/slide_wir-ueber-uns_01.a9dfbf11.jpg"
+  }
+]);
